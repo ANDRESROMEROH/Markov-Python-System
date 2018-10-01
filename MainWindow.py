3a4f6faf0c5610c2ec80da53be50c52192d19d45
Estefania Murillo <estefania.murillo.romero@est.una.ac.cr>
2018-09-30 20:17:08 -0700

    The palette is done.

--- MainWindow.py
+++ MainWindow.py
@@ -1,12 +1,15 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
-import os
+#!/usr/bin/python
+#  # -*- coding: U+0395 -*-
+import os, sys
+
 
 class Ui_MainWindow(object):
     
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(802, 632)
+        MainWindow.resize(1022, 632)
 
         self.currentFile = ""
 
@@ -27,7 +30,9 @@ class Ui_MainWindow(object):
         self.runBt.setAutoFillBackground(False)
         self.runBt.setObjectName("runBt")
         MainWindow.setCentralWidget(self.centralwidget)
-
+        self.groupPalette = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupPalette.setGeometry(QtCore.QRect(800, 30, 211, 271))
+        self.groupPalette.setObjectName("groupPalette")
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 802, 22))
         self.menubar.setObjectName("menubar")
@@ -70,14 +75,119 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuInput.menuAction())
         self.menubar.addAction(self.menuTools.menuAction())
 
-        self.retranslateUi(MainWindow)
+        #Palette
+        self.alfabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.alfabtn.setGeometry(QtCore.QRect(10, 30, 31, 31))
+        self.alfabtn.setObjectName("alfabtn")
+        self.betabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.betabtn.setGeometry(QtCore.QRect(50, 30, 31, 31))
+        self.betabtn.setObjectName("betabtn")
+        self.gammabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.gammabtn.setGeometry(QtCore.QRect(90, 30, 31, 31))
+        self.gammabtn.setObjectName("gammabtn")
+        self.deltabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.deltabtn.setGeometry(QtCore.QRect(130, 30, 31, 31))
+        self.deltabtn.setObjectName("deltabtn")
+        self.epsilonbtn = QtWidgets.QPushButton(self.groupPalette)
+        self.epsilonbtn.setGeometry(QtCore.QRect(170, 30, 31, 31))
+        self.epsilonbtn.setObjectName("epsilonbtn")
+        self.dsetabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.dsetabtn.setGeometry(QtCore.QRect(10, 70, 31, 31))
+        self.dsetabtn.setObjectName("dsetabtn")
+        self.etabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.etabtn.setGeometry(QtCore.QRect(50, 70, 31, 31))
+        self.etabtn.setObjectName("etabtn")
+        self.thetabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.thetabtn.setGeometry(QtCore.QRect(90, 70, 31, 31))
+        self.thetabtn.setObjectName("thetabtn")
+        self.iotabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.iotabtn.setGeometry(QtCore.QRect(130, 70, 31, 31))
+        self.iotabtn.setObjectName("iotabtn")
+        self.kappabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.kappabtn.setGeometry(QtCore.QRect(170, 70, 31, 31))
+        self.kappabtn.setObjectName("kappabtn")
+        self.lambdaminbtn = QtWidgets.QPushButton(self.groupPalette)
+        self.lambdaminbtn.setGeometry(QtCore.QRect(10, 110, 31, 31))
+        self.lambdaminbtn.setObjectName("lambdaminbtn")
+        self.lambdamaybtn = QtWidgets.QPushButton(self.groupPalette)
+        self.lambdamaybtn.setGeometry(QtCore.QRect(50, 110, 31, 31))
+        self.lambdamaybtn.setObjectName("lambdamaybtn")
+        self.mybtn = QtWidgets.QPushButton(self.groupPalette)
+        self.mybtn.setGeometry(QtCore.QRect(90, 110, 31, 31))
+        self.mybtn.setObjectName("mybtn")
+        self.nybtn = QtWidgets.QPushButton(self.groupPalette)
+        self.nybtn.setGeometry(QtCore.QRect(130, 110, 31, 31))
+        self.nybtn.setObjectName("nybtn")
+        self.xibtn = QtWidgets.QPushButton(self.groupPalette)
+        self.xibtn.setGeometry(QtCore.QRect(170, 110, 31, 31))
+        self.xibtn.setObjectName("xibtn")
+        self.omicronbtn = QtWidgets.QPushButton(self.groupPalette)
+        self.omicronbtn.setGeometry(QtCore.QRect(10, 150, 31, 31))
+        self.omicronbtn.setObjectName("omicronbtn")
+        self.pibtn = QtWidgets.QPushButton(self.groupPalette)
+        self.pibtn.setGeometry(QtCore.QRect(50, 150, 31, 31))
+        self.pibtn.setObjectName("pibtn")
+        self.rhobtn = QtWidgets.QPushButton(self.groupPalette)
+        self.rhobtn.setGeometry(QtCore.QRect(90, 150, 31, 31))
+        self.rhobtn.setObjectName("rhobtn")
+        self.sigmabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.sigmabtn.setGeometry(QtCore.QRect(130, 150, 31, 31))
+        self.sigmabtn.setObjectName("sigmabtn")
+        self.taubtn = QtWidgets.QPushButton(self.groupPalette)
+        self.taubtn.setGeometry(QtCore.QRect(170, 150, 31, 31))
+        self.taubtn.setObjectName("taubtn")
+        self.ypsilonbtn = QtWidgets.QPushButton(self.groupPalette)
+        self.ypsilonbtn.setGeometry(QtCore.QRect(10, 190, 31, 31))
+        self.ypsilonbtn.setObjectName("ypsilonbtn")
+        self.fibtn = QtWidgets.QPushButton(self.groupPalette)
+        self.fibtn.setGeometry(QtCore.QRect(50, 190, 31, 31))
+        self.fibtn.setObjectName("fibtn")
+        self.jibtn = QtWidgets.QPushButton(self.groupPalette)
+        self.jibtn.setGeometry(QtCore.QRect(90, 190, 31, 31))
+        self.jibtn.setObjectName("jibtn")
+        self.psibtn = QtWidgets.QPushButton(self.groupPalette)
+        self.psibtn.setGeometry(QtCore.QRect(130, 190, 31, 31))
+        self.psibtn.setObjectName("psibtn")
+        self.omegabtn = QtWidgets.QPushButton(self.groupPalette)
+        self.omegabtn.setGeometry(QtCore.QRect(170, 190, 31, 31))
+        self.omegabtn.setObjectName("omegabtn")
+        self.arrowbtn = QtWidgets.QPushButton(self.groupPalette)
+        self.arrowbtn.setGeometry(QtCore.QRect(60, 230, 89, 25))
+        self.arrowbtn.setObjectName("arrowbtn")
 
         #FILE Menu Management:
         self.actionNew.triggered.connect(self.file_new)
         self.actionOpen.triggered.connect(self.file_open)
         self.actionSave.triggered.connect(self.file_save)
         self.actionSaveAs.triggered.connect(self.file_saveAs)
+        self.alfabtn.clicked.connect(self.putAlfa)
+        self.betabtn.clicked.connect(self.putBeta)
+        self.gammabtn.clicked.connect(self.putGamma)
+        self.deltabtn.clicked.connect(self.putDelta)
+        self.epsilonbtn.clicked.connect(self.putEpsilon)
+        self.dsetabtn.clicked.connect(self.putDseta)
+        self.etabtn.clicked.connect(self.putEta)
+        self.thetabtn.clicked.connect(self.putTheta)
+        self.iotabtn.clicked.connect(self.putIota)
+        self.kappabtn.clicked.connect(self.putKappa)
+        self.lambdaminbtn.clicked.connect(self.putLambdaMin)
+        self.lambdamaybtn.clicked.connect(self.putLambdaMay)
+        self.mybtn.clicked.connect(self.putMy)
+        self.nybtn.clicked.connect(self.putNy)
+        self.xibtn.clicked.connect(self.putXi)
+        self.omicronbtn.clicked.connect(self.putOmicron)
+        self.pibtn.clicked.connect(self.putPi)
+        self.rhobtn.clicked.connect(self.putRho)
+        self.sigmabtn.clicked.connect(self.putSigma)
+        self.taubtn.clicked.connect(self.putTau)
+        self.ypsilonbtn.clicked.connect(self.putYpsilon)
+        self.fibtn.clicked.connect(self.putFi)
+        self.jibtn.clicked.connect(self.putJi)
+        self.psibtn.clicked.connect(self.putPsi)
+        self.omegabtn.clicked.connect(self.putOmega)
+        self.arrowbtn.clicked.connect(self.putArrow)
         
+        self.retranslateUi(MainWindow)
 
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         self.mainWindow = MainWindow
@@ -98,6 +208,33 @@ class Ui_MainWindow(object):
         self.actionSimple_Input.setText(_translate("MainWindow", "Simple Input"))
         self.actionMultiple_Inputs.setText(_translate("MainWindow", "Multiple Inputs"))
         self.actionPalette.setText(_translate("MainWindow", "Palette"))
+        self.groupPalette.setTitle(_translate("MainWindow", "Palette"))
+        self.alfabtn.setText(_translate("MainWindow", "\u03B1"))
+        self.betabtn.setText(_translate("MainWindow", "\u03B2"))
+        self.gammabtn.setText(_translate("MainWindow", "\u03B3"))
+        self.deltabtn.setText(_translate("MainWindow", "\u03B4"))
+        self.epsilonbtn.setText(_translate("MainWindow", "\u03B5"))
+        self.dsetabtn.setText(_translate("MainWindow", "\u03B6"))
+        self.etabtn.setText(_translate("MainWindow", "\u03B7"))
+        self.thetabtn.setText(_translate("MainWindow", "\u03B8"))
+        self.iotabtn.setText(_translate("MainWindow", "\u03B9"))
+        self.kappabtn.setText(_translate("MainWindow", "\u03BA"))
+        self.lambdaminbtn.setText(_translate("MainWindow", "\u03BB"))
+        self.lambdamaybtn.setText(_translate("MainWindow", "\u039B"))
+        self.mybtn.setText(_translate("MainWindow", "\u03BC"))
+        self.nybtn.setText(_translate("MainWindow", "\u03BD"))
+        self.xibtn.setText(_translate("MainWindow", "\u03BE"))
+        self.omicronbtn.setText(_translate("MainWindow", "\u03BF"))
+        self.pibtn.setText(_translate("MainWindow", "\u03C0"))
+        self.rhobtn.setText(_translate("MainWindow", "\u03C1"))
+        self.sigmabtn.setText(_translate("MainWindow", "\u03C3"))
+        self.taubtn.setText(_translate("MainWindow", "\u03C4"))
+        self.ypsilonbtn.setText(_translate("MainWindow", "\u03C5"))
+        self.fibtn.setText(_translate("MainWindow", "\u03C6"))
+        self.jibtn.setText(_translate("MainWindow", "\u03C7"))
+        self.psibtn.setText(_translate("MainWindow", "\u03C8"))
+        self.omegabtn.setText(_translate("MainWindow", "\u03C9"))
+        self.arrowbtn.setText(_translate("MainWindow", "\u2192"))
 
 
     #File Management Actions:
@@ -165,7 +302,113 @@ class Ui_MainWindow(object):
         self.currentFile = ""
         self.mainWindow.setWindowTitle("Untitled")
 
+    def putAlfa(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B1")
+        cursor.movePosition(cursor.Right, cursor.KeepAnchor,  3)
     
+    def putBeta(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B2")
+        cursor.movePosition(cursor.Right, cursor.KeepAnchor,  3)
+
+    def putGamma(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B3")
+        cursor.movePosition(cursor.Right, cursor.KeepAnchor,  3)
+
+    def putDelta(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B4")
+        cursor.movePosition(cursor.Right, cursor.KeepAnchor,  3)
+
+    def putEpsilon(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B5")
+
+    def putDseta(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B6")
+
+    def putEta(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B7")
+
+    def putTheta(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B8")
+
+    def putIota(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03B9")
+
+    def putKappa(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03BA")
+    
+    def putLambdaMin(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03BB")
+    
+    def putLambdaMay(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u039B")
+
+    def putMy(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03BC")
+    
+    def putNy(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03BD")
+    
+    def putXi(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03BE")
+
+    def putOmicron(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03BF")
+
+    def putPi(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C0")
+
+    def putRho(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C1")
+    
+    def putSigma(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C3")
+    
+    def putTau(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C4")
+
+    def putYpsilon(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C5")
+
+    def putFi(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C6")
+    
+    def putJi(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C7")
+
+    def putPsi(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C8")
+    
+    def putOmega(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u03C9")
+
+    def putArrow(self):
+        cursor = self.plainTextEdit.textCursor()
+        self.plainTextEdit.insertPlainText("\u2192")
 
 if __name__ == "__main__":
     import sys
