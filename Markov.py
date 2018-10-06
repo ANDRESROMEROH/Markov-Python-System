81b5b0b8088dcb5623fae8c9ac7421183f2486ea
Andrés Romero <andres.romero22@outlook.com>
2018-10-06 11:21:07 -0600

    Symbols, Vars and Markers Recognition

--- Markov.py
+++ Markov.py
@@ -1,26 +1,38 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
-import os, sys
+import os, sys, re
 
 from MainWindow import Ui_MainWindow
 
 
 class Markov(QMainWindow, Ui_MainWindow):
 
+
     symbols = []
     markers = []
     variables = []
+
 
     def __init__(self, *args, **kwargs):
         super(Markov, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.show()
-        self.runBt.clicked.connect(self.test)
+        self.runBt.clicked.connect(self.load_attributes)
 
-    def test(self):
-        text = str(self.plainTextEdit.toPlainText())
-        lines = text.splitlines()
-        print(lines[0].partition(" ")[2].lstrip())
+
+    def load_attributes(self):
+        lines = str(self.plainTextEdit.toPlainText()).splitlines()
+
+
+        for line in lines:
+            if re.match("#symbols", line):
+                symbols = re.sub("^\#symbols", "", line).lstrip()
+            else:
+                if re.match("#vars", line):
+                    variables = re.sub("^\#vars", "", line).lstrip()
+                else:
+                    if re.match("#markers", line):
+                        markers = re.sub("^\#markers", "", line).lstrip()
 
 
 
