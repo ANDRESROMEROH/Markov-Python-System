363d7aa83e9816772a5e564ec828d6a3c9e86675
Andrés Romero <andres.romero22@outlook.com>
2018-10-09 20:13:40 -0600

    Create Rule() New Function

--- Markov.py
+++ Markov.py
@@ -1,63 +1,132 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
-import os, sys, re
+import os, sys, re, codecs
 from classes import Rule
-
 from MainWindow import Ui_MainWindow
 
 
 class Markov(QMainWindow, Ui_MainWindow):
 
-    symbols = []
-    markers = []
-    variables = [] 
-    rules = []
-
     def __init__(self, *args, **kwargs):
         super(Markov, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.show()
+
+        self.variables = [] 
+        self.symbols = []
+        self.markers = []
+        self.rules = []
+
         self.runBt.clicked.connect(self.run)
+
 
     def run(self):
         self.load_attributes()
-
+    
 
     def load_attributes(self):
         lines = str(self.plainTextEdit.toPlainText()).splitlines()
         
         for line in lines:
             if re.match("#symbols", line):
-                symbols = re.sub("^\#symbols", "", line).lstrip()
+                self.symbols = re.sub("^\#symbols", "", line).lstrip()
             else:
                 if re.match("#vars", line):
-                    variables = re.sub("^\#vars", "", line).lstrip()
+                    self.variables = re.sub("^\#vars", "", line).lstrip()
                 else:
                     if re.match("#markers", line):
-                        markers = re.sub("^\#markers", "", line).lstrip()
+                        self.markers = re.sub("^\#markers", "", line).lstrip()
                     else:
-                        if re.match("^\w+\:", line):
+                        if re.match("^.+", line):
                             self.create_rule(line)
+        
 
-    
-    def create_rule(self, line): #Creates a new Rule() object, entry example: P1:βx → xβ (P1)
+    def create_rule(self, line):
         rule = Rule()
 
-        rule.id = line.split(":")[0] #Splits the ID, ID=P1
-        line = line.split(":")[1].split("\u2192") #Splits the pattern,  pattern=βx
-        rule.pattern = line[0] 
-        rule.substitution = line[1].split("(")[0].lstrip() #splits the subs, substitution=xβ
+        #ID
+        forId = re.compile('([\D].*)(?=:)') #Regular expresion to extract identifiers
+        reMatch = forId.search(line)
+        if reMatch: #If the rule has an id
+            rule.id = reMatch.group(0).lstrip()
 
-        if re.search("\(\w+\)", line[1]):
-            rule.tag = line[1].split("(")[1].replace(')', '') #splits the tag, tag=P1
+        #Pattern
+        compilerNoIdQuotes = re.compile('(?<=\")(.*)(?=\"\s*\→)', re.IGNORECASE | re.UNICODE) #Compiler for pattern with no id, ex: "x"->
+        compilerNoId = re.compile('(.*)(?=→)', re.IGNORECASE | re.UNICODE) #Compiler for pattern with no id, ex: x->
+        compilerId = re.compile('(?<=:)(.*)(?=→)', re.IGNORECASE | re.UNICODE) #Compiler for pattern with id, ex: P1: XX->
+        
+        reMatchNoIdQuotes = compilerNoIdQuotes.search(line)
+        reMatchNoId = compilerNoId.search(line)
+        reMatchId = compilerId.search(line)
 
-        if re.search("\.$",line[1]): #Look up if the given rule is final
-            rule.isFinal=True
+        #print("TEST")
 
-        rules.append(rule)
+        if reMatchId:
+            rule.pattern = reMatchId.group(0).lstrip()
+        else:
+            if reMatchNoIdQuotes:
+                rule.pattern = reMatchNoIdQuotes.group(0).lstrip()
+                
+            else:
+                if reMatchNoId:
+                    rule.pattern = reMatchNoId.group(0).lstrip()
+
+        #Substitution and isFinal
+        compilerTag= re.compile('(?<=→)(.*)(?=\()', re.IGNORECASE | re.UNICODE) #Compiler for substitution with a tag
+        compilerPoint = re.compile('(?<=→)(.*)(?=\.)', re.IGNORECASE | re.UNICODE) #Compiler for final substitution
+        compilerSubs = re.compile('(?<=→)(.*)', re.IGNORECASE | re.UNICODE) #Compiler for just a substitution 
+        
+        reMatchTag = compilerTag.search(line)
+        reMatchPoint = compilerPoint.search(line)
+        reMatchSubs = compilerSubs.search(line)
+
+        if reMatchTag:
+            rule.substitution = reMatchTag.group(0).lstrip()
+        else:
+            if reMatchPoint:
+                rule.substitution = reMatchPoint.group(0).lstrip()
+                rule.isFinal=True
+                
+            else:
+                if reMatchSubs:
+                    #print(" Tercer IF nothing")
+                    rule.substitution = reMatchSubs.group(0).lstrip()
+        
+        #Tag
+        tag = re.compile('(?<=\()(.*)(?=\))') #Regular expresion to extract identifiers
+        reMatchForTag = tag.search(line)
+        if reMatchForTag : #If the rule has an id
+            rule.tag = reMatchForTag.group(0).lstrip()
+
+        self.rules.append(rule)    
+                    
+        # if (rule.id!=None):
+        #     print("Id:"+rule.id)
+        # print("Pattern: "+rule.pattern)
+        # print("Substitution: "+rule.substitution)
+        # if (rule.tag!=None):
+        #     print("Tag:"+rule.tag)
+        # print("Is final? ")
+        # print(rule.isFinal)
+        # print("*****************************")
 
 
-########## Application Main:
+
+    def replace(self, input):
+        for rule in self.rules:
+            if rule.pattern in input:
+                input = input.replace(rule.pattern, rule.substitution, 1)
+                if rule.isFinal:
+                    #return input
+                    print(input)
+                break
+        else:
+            #return input
+            print("False")
+
+
+
+############ Application Main #############
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     markov = Markov()
--- classes/rule.py
+++ classes/rule.py
@@ -4,18 +4,8 @@ class Rule(object):
 
     def __init__(self):
         #Attributes:
-        self.id = ""
+        self.id = None
         self.pattern = ""
         self.substitution = ""
         self.tag = None
         self.isFinal = False
-
-    def is_final_rule(self):
-        return self.isFinal
-    
-
-# if __name__ == '__main__':
-#     test = Rule()
-#     test.id = "p1"
-#     print(test.id)
-
